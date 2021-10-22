1c1cfbd6b1b1a820a9e8238526a321bc9cc419ee
iggafebrianv <iggafebrian4@gmail.com>
2021-10-22 17:25:32 +0700

    Package and Class

---
+++ SisfoSekolah/src/Sekolah2/nilai.java
@@ -0,0 +1,12 @@
+/*
+Topik   : Sekolah
+Ketua   : Arpriansah Yonathan (1301194112)
+Anggota : Balqis Sayyidahtul Atikah (1301193480)
+          Igga Febrian Virgiani (1301194283)
+          Manuel Benedict (1301194182)
+ */
+package Sekolah2;
+
+public class nilai {
+    
+}
