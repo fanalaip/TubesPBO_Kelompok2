acf63e6e9a6cdaba95b9f06d9b9d987a5a6d46c8
iggafebrianv <iggafebrian4@gmail.com>
2021-10-22 17:28:27 +0700

    main driver

---
+++ SisfoSekolah/src/MainDriver/DriverSekolah.java
@@ -0,0 +1,16 @@
+/*
+Topik   : Sekolah
+Ketua   : Arpriansah Yonathan (1301194112)
+Anggota : Balqis Sayyidahtul Atikah (1301193480)
+          Igga Febrian Virgiani (1301194283)
+          Manuel Benedict (1301194182)
+ */
+package MainDriver;
+
+public class DriverSekolah {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
