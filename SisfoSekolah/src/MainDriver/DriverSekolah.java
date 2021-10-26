97fc8c83f8944da133b051838a834702a1e7675e
iggafebrianv <iggafebrian4@gmail.com>
2021-10-27 06:50:11 +0700

    update

--- SisfoSekolah/src/MainDriver/DriverSekolah.java
+++ SisfoSekolah/src/MainDriver/DriverSekolah.java
@@ -1,9 +1,10 @@
 /*
 Topik   : Sekolah
-Ketua   : Arpriansah Yonathan (1301194112)
-Anggota : Balqis Sayyidahtul Atikah (1301193480)
+Anggota : Arpriansah Yonathan (1301194112)
+          Balqis Sayyidahtul Atikah (1301193480)
           Igga Febrian Virgiani (1301194283)
           Manuel Benedict (1301194182)
+Kelas : IF-43-05
  */
 package MainDriver;
 
