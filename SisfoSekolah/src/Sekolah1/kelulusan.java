ba14a602ba7af7e5ff89fad84e86b3cb575f59e6
iggafebrianv <iggafebrian4@gmail.com>
2021-10-27 06:48:41 +0700

    update

--- SisfoSekolah/src/Sekolah1/kelulusan.java
+++ SisfoSekolah/src/Sekolah1/kelulusan.java
@@ -1,13 +1,11 @@
 /*
 Topik   : Sekolah
-Ketua   : Arpriansah Yonathan (1301194112)
-Anggota : Balqis Sayyidahtul Atikah (1301193480)
+Anggota : Arpriansah Yonathan (1301194112)
+          Balqis Sayyidahtul Atikah (1301193480)
           Igga Febrian Virgiani (1301194283)
           Manuel Benedict (1301194182)
  */
 package Sekolah1;
-
-import Sekolah1.siswa;
 
 public class kelulusan{
     private int semester;
