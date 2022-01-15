c3f72314352a914f9fea4e1600342d6c39e86026
iggafebrianv <iggafebrian4@gmail.com>
2022-01-15 17:31:13 +0700

    takudz

--- SisfoSekolah/src/Controller/ControllerGuru.java
+++ SisfoSekolah/src/Controller/ControllerGuru.java
@@ -10,9 +10,11 @@ package Controller;
 import GUI.guiGuru;
 import Model.Database;
 import Model.guru;
+import Model.siswa;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import javax.swing.JOptionPane;
 
 public class ControllerGuru extends MouseAdapter implements ActionListener {
     private guiGuru viewGuru;
@@ -27,9 +29,67 @@ public class ControllerGuru extends MouseAdapter implements ActionListener {
         //combobox();
         viewGuru.setVisible(true);
     }
+    public String[] getMatkul() {
+        if (db.getListMapel().size() <= 0){
+            JOptionPane.showMessageDialog(null, "Mata pelajaran Kosong");
+        } else {
+            String[] listMapel = new String[db.getListMapel().size()];
+            for (int i = 0; i < listMapel.length; i++) {
+                listMapel[i] = db.getListMapel().get(i).getMataPelajaran();
+            }
+        }
+        return null;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         Object source = e.getSource();
+        try {
+            if (source.equals(viewGuru.getBtnBack())) {
+                new Controller();
+                viewGuru.dispose();
+            } 
+            else if (source.equals(viewGuru.getBtnAdd())) {
+                try {
+                        String nama = viewGuru.getNama();
+                        String nis = viewGuru.getTfNIS();
+                        String id_mapel = viewGuru.getCbMapelText();
+                        if (nama == null || nis == null || id_mapel == null) {
+                            JOptionPane.showMessageDialog(viewGuru, "Input Belum Benar");
+                        }else{
+                            int i = getRollNum() + 1;
+                            siswa mhs = new siswa(nis, nama);
+                            mhs.addMhs(db);
+                            mhs.addJadwal(id_jadwal, i, db);
+                            mhs.addMatkul(id_jadwal, db);
+                            viewGuru.resetviewGuru();  
+                        }
+                                     
+                } catch (Exception es) {
+                    JOptionPane.showMessageDialog(null, "input salah") ; 
+                    es.printStackTrace();
+                 
+                }
+            }
+        } catch (Exception ef) {
+            JOptionPane.showMessageDialog(null, "Error");
+        }
     }
+    
+     public int getRollNum() {
+        int i = 0;
+        try {
+            db.connect();
+            String sql = "SELECT no_enroll FROM enroll ORDER BY no_enroll ASC";
+            db.setRs(db.getStmt().executeQuery(sql));
+            while (db.getRs().next()) {                
+                i = db.getRs().getInt("no_enroll");
+            }
+            db.disconnect();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return i;
+    }
+    
 }
