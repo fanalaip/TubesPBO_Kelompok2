e8c4a1286d1ecfe3f323f8cf9990932f19d9d9fd
iggafebrianv <iggafebrian4@gmail.com>
2022-01-16 19:22:10 +0700

    dikit2

--- SisfoSekolah/src/Controller/ControllerAdmin.java
+++ SisfoSekolah/src/Controller/ControllerAdmin.java
@@ -33,6 +33,11 @@ public class ControllerAdmin extends MouseAdapter implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object source = e.getSource();
+            try {
+                if (source.equals(viewSiswa.getButtonBack())) {
+                    new Controller();
+                    viewSiswa.dispose();
+                } 
     }
 }
